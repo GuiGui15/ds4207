f5db0b182f73f1898d72dd1063372141c111c47c
GuiGui15 <guilherme.calpena@gmail.com>
2022-04-15 11:50:45 +0200

    dernierPushDevoir

--- src/Controller/TacheController.php
+++ src/Controller/TacheController.php
@@ -9,9 +9,55 @@ use Symfony\Component\Routing\Annotation\Route;
 class TacheController extends AbstractController
 {
     /**
-     * @Route("/tache", name="app_tache")
+     * @Route("/tache", name="add_tache")
      */
     public function index(): Response
+    {
+        return $this->render('tache/index.html.twig', [
+            'controller_name' => 'TacheController',
+        ]);
+    }
+
+    /**
+     * @Route("/ListeTaches", name="liste")
+     */
+    public function ListeTaches(): Response
+    {
+        return $this->render('tache/index.html.twig', [
+            'controller_name' => 'TacheController',
+        ]);
+    }
+    /**
+     * @Route("/tache", name="add_tache")
+     */
+    public function taches(): Response
+    {
+        return $this->render('tache/index.html.twig', [
+            'controller_name' => 'TacheController',
+        ]);
+    }
+    /**
+     * @Route("/suprtaches", name="suppr")
+     */
+    public function suprtaches(): Response
+    {
+        return $this->render('tache/index.html.twig', [
+            'controller_name' => 'TacheController',
+        ]);
+    }
+    /**
+     * @Route("/creertaches", name="creer")
+     */
+    public function creertaches(): Response
+    {
+        return $this->render('tache/index.html.twig', [
+            'controller_name' => 'TacheController',
+        ]);
+    }
+    /**
+     * @Route("/inserttaches", name="insert")
+     */
+    public function insertaches(): Response
     {
         return $this->render('tache/index.html.twig', [
             'controller_name' => 'TacheController',
